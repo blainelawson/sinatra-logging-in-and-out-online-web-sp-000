5cb6b63b5ec732bba5f50903c8da7a619a28464f
Blaine Lawson <blaine.lawson@gmail.com>
2019-05-17 23:18:11 +0000

    Automatically backed up by Learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -28,11 +28,11 @@ class ApplicationController < Sinatra::Base
   end
 
   get '/account' do
-    @user = User.find(session[:user_id])
+    @user = User.find_by_id(session[:user_id])
     if @user
       erb :account
     else
-      redirect '/error'
+      erb :error
     end
   end
 
